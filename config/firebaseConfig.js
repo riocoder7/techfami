dc29f6abd462c175ad4e37a825ffe4ad7d9377a3
riocoder7 <sarfaraztech07@gmail.com>
2025-08-10 08:52:18 +0530

    update review section

---
+++ config/firebaseConfig.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCBGIq3zwxbPWd-Xp5YxlksOroVLITlwuU",
+    authDomain: "techfami-1d931.firebaseapp.com",
+    projectId: "techfami-1d931",
+    storageBucket: "techfami-1d931.firebasestorage.app",
+    messagingSenderId: "517900608019",
+    appId: "1:517900608019:web:cf32043193d541134d55e6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
